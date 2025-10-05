5345279a4df715b2c20059670784bbf4c3b8b02c
Charlesss-0 <aragonaugusto588@gmail.com>
2025-10-04 21:17:16 -0600

    fix URL formatting for sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
 	return [
 		{
-			url: 'skyline-adventures.vercel.app',
+			url: 'https://skyline-adventures.vercel.app',
 			lastModified: new Date(),
 			changeFrequency: 'yearly',
 			priority: 1,
 		},
 		{
-			url: 'skyline-adventures.vercel.app/destinations',
+			url: 'https://skyline-adventures.vercel.app/destinations',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 0.8,
 		},
 		{
-			url: 'skyline-adventures.vercel.app/about',
+			url: 'https://skyline-adventures.vercel.app/about',
 			lastModified: new Date(),
 			changeFrequency: 'monthly',
 			priority: 0.5,
